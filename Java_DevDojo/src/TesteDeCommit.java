fdc17d5482be717b0585e9d5118062292a0c1711
Gezildo Anacleto <gezildo.anacleto@lumetec.com.br>
2020-10-22 11:08:35 -0300

    Teste de commit

--- Java_DevDojo/src/TesteDeCommit.java
+++ Java_DevDojo/src/TesteDeCommit.java
@@ -1,5 +1,5 @@
 public class TesteDeCommit {
     public static void main(String[] args) {
-        System.out.println("Esse é um teste de commit DevDojo");
+        System.out.println("Esse é um teste de commit DevDojo diretamente do InteliJ");
     }
 }
